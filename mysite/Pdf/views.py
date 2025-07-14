2b018125a680d2a35400e5c7c976c376eea9b211
Mann180 <mannmehta01@gmail.com>
2025-07-14 15:59:55 +0530

    homepage

--- mysite/Pdf/views.py
+++ mysite/Pdf/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def accept_data(request):
+    return render(request,'pdf/accept_data.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Pdf.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',accept_data,name='accept_data'),
 ]
